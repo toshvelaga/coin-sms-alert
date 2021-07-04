5abcf2c92f3c58ae60868ffe823b64f15bb6e266
Tosh Velaga <toshvelaga@gmail.com>
2021-07-04 04:43:17 -0500

    edit server

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const app = express();
 app.set('json spaces', 2)
 
 // Start listening on port 3000 to start receiving requests
-app.listen(3000, () => {
-   console.log('Coin Alert app listening on port 3000!');
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+   console.log('Coin SMS Alert app listening on port 3000!');
 
     // Set up routes
     routes.configure(app);
